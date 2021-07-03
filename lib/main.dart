eeb5608ef789d0f264d29fc43d6f6086512fccd4
Cp1337 <cyril199897@gmail.com>
2021-07-03 21:49:08 +0530

    Qr basic scannign

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moriarity/Widgets/QrCode.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: QRViewExample(),
     );
   }
 }
